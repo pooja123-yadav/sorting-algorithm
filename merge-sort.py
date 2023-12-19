cbe5eaf3b147470a09adfa93163d2837c636d8a1
pooja123-yadav <85959414+pooja123-yadav@users.noreply.github.com>
2023-12-19 12:27:56 +0530

    Update merge-sort.py

--- merge-sort.py
+++ merge-sort.py
@@ -1,1 +1,3 @@
-
+# Merge sort works by splitting the input list into two halves, repeating the process on those halves, and finally merging the two sorted halves
+# together. The algorithm first moves from top to bottom, dividing the list into smaller and smaller parts until only the separate elements
+# remain.
